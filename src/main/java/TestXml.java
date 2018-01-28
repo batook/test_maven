7c9338a581bfa428a9493a7e1e1fd4da37ec8ff1
batook <batook@mail.ru>
2018-01-28 14:12:44 +0300

    test

--- src/main/java/TestXml.java
+++ src/main/java/TestXml.java
@@ -1,13 +1,403 @@
+import org.w3c.dom.*;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.*;
+import javax.xml.stream.*;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.xpath.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+//DOM Reader
+class DomExample {
+    public static void main(String[] args) {
+        try {
+            // Создается построитель документа
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            // Создается дерево DOM документа из файла
+            Document document = documentBuilder.parse("BookCatalog.xml");
 
+            // Получаем корневой элемент
+            Node root = document.getDocumentElement();
+
+            System.out.println("List of books:");
+            System.out.println();
+            // Просматриваем все подэлементы корневого - т.е. книги
+            NodeList books = root.getChildNodes();
+            for (int i = 0; i < books.getLength(); i++) {
+                Node book = books.item(i);
+                // Если нода не текст, то это книга - заходим внутрь
+                if (book.getNodeType() != Node.TEXT_NODE) {
+                    NodeList bookProps = book.getChildNodes();
+                    for(int j = 0; j < bookProps.getLength(); j++) {
+                        Node bookProp = bookProps.item(j);
+                        // Если нода не текст, то это один из параметров книги - печатаем
+                        if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(0).getTextContent());
+                        }
+                    }
+                    System.out.println("===========>>>>");
+                }
+            }
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace(System.out);
+        } catch (SAXException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
+
+//DOM Writer
+class DomExample2 {
+
+    public static void main(String[] args) {
+        try {
+            // Создается построитель документа
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            // Создается дерево DOM документа из файла
+            Document document = documentBuilder.parse("BookCatalog.xml");
+
+            // Вызываем метод для добавления новой книги
+            addNewBook(document);
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace(System.out);
+        } catch (SAXException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    // Функция добавления новой книги и записи результата в файл
+    private static void addNewBook(Document document) throws TransformerFactoryConfigurationError, DOMException {
+        // Получаем корневой элемент
+        Node root = document.getDocumentElement();
+
+        // Создаем новую книгу по элементам
+        // Сама книга <Book>
+        Element book = document.createElement("Book");
+        // <Title>
+        Element title = document.createElement("Title");
+        // Устанавливаем значение текста внутри тега
+        title.setTextContent("Incredible book about Java");
+        // <Author>
+        Element author = document.createElement("Author");
+        author.setTextContent("Saburov Anton");
+        // <Date>
+        Element date = document.createElement("Date");
+        date.setTextContent("2015");
+        // <ISBN>
+        Element isbn = document.createElement("ISBN");
+        isbn.setTextContent("0-06-999999-9");
+        // <Publisher>
+        Element publisher = document.createElement("Publisher");
+        publisher.setTextContent("Java-Course publisher");
+        // <Cost>
+        Element cost = document.createElement("Cost");
+        cost.setTextContent("499");
+        // Устанавливаем атрибут
+        cost.setAttribute("currency", "RUB");
+
+        // Добавляем внутренние элементы книги в элемент <Book>
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(date);
+        book.appendChild(isbn);
+        book.appendChild(publisher);
+        book.appendChild(cost);
+        // Добавляем книгу в корневой элемент
+        root.appendChild(book);
+
+        // Записываем XML в файл
+        writeDocument(document);
+    }
+
+    // Функция для сохранения DOM в файл
+    private static void writeDocument(Document document) throws TransformerFactoryConfigurationError {
+        try {
+            Transformer tr = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(document);
+            FileOutputStream fos = new FileOutputStream("other.xml");
+            StreamResult result = new StreamResult(fos);
+            tr.transform(source, result);
+        } catch (TransformerException | IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
+
+//SAX Parser
+class SaxExample {
+    public static void main(String args[]) {
+        // Имя файла
+        final String fileName = "Phonebook.xml";
+
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+
+            // Здесь мы определили анонимный класс, расширяющий класс DefaultHandler
+            DefaultHandler handler = new DefaultHandler() {
+                // Поле для указания, что тэг NAME начался
+                boolean name = false;
+
+                // Метод вызывается когда SAXParser "натыкается" на начало тэга
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    // Если тэг имеет имя NAME, то мы этот момент отмечаем - начался тэг NAME
+                    if (qName.equalsIgnoreCase("NAME")) {
+                        name = true;
+                    }
+                }
+
+                // Метод вызывается когда SAXParser считывает текст между тэгами
+                @Override
+                public void characters(char ch[], int start, int length) throws SAXException {
+                    // Если перед этим мы отметили, что имя тэга NAME - значит нам надо текст использовать.
+                    if (name) {
+                        System.out.println("Name: " + new String(ch, start, length));
+                        name = false;
+                    }
+                }
+            };
+
+            // Стартуем разбор методом parse, которому передаем наследника от DefaultHandler, который будет вызываться в нужные моменты
+            saxParser.parse(fileName, handler);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//StAX Reader
+class StaxExample {
+    public static void main(String[] args) {
+        final String fileName = "BookCatalogue.xml";
+        try {
+            XMLStreamReader xmlr = XMLInputFactory.newInstance().createXMLStreamReader(fileName, new FileInputStream(fileName));
+
+            while (xmlr.hasNext()) {
+                xmlr.next();
+                if (xmlr.isStartElement()) {
+                    System.out.println(xmlr.getLocalName());
+                } else if (xmlr.isEndElement()) {
+                    System.out.println("/" + xmlr.getLocalName());
+                } else if (xmlr.hasText() && xmlr.getText().trim().length() > 0) {
+                    System.out.println("   " + xmlr.getText());
+                }
+            }
+        } catch (FileNotFoundException | XMLStreamException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+//StAX Writer
+class StaxWriteExample {
+
+    public static void main(String[] args) {
+        try {
+            XMLOutputFactory output = XMLOutputFactory.newInstance();
+            XMLStreamWriter writer = output.createXMLStreamWriter(new FileWriter("result.xml"));
+
+            // Открываем XML-документ и Пишем корневой элемент BookCatalogue
+            writer.writeStartDocument("1.0");
+            writer.writeStartElement("BookCatalogue");
+            // Делаем цикл для книг
+            for (int i = 0; i < 5; i++) {
+                // Записываем Book
+                writer.writeStartElement("Book");
+
+                // Заполняем все тэги для книги
+                // Title
+                writer.writeStartElement("Title");
+                writer.writeCharacters("Book #" + i);
+                writer.writeEndElement();
+                // Author
+                writer.writeStartElement("Author");
+                writer.writeCharacters("Author #" + i);
+                writer.writeEndElement();
+                // Date
+                writer.writeStartElement("Date");
+                writer.writeCharacters(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+                writer.writeEndElement();
+                // ISBN
+                writer.writeStartElement("ISBN");
+                writer.writeCharacters("ISBN #" + i);
+                writer.writeEndElement();
+                // Publisher
+                writer.writeStartElement("Publisher");
+                writer.writeCharacters("Publisher #" + i);
+                writer.writeEndElement();
+                // Cost
+                writer.writeStartElement("Cost");
+                writer.writeAttribute("currency", "USD");
+                writer.writeCharacters("" + (i + 10));
+                writer.writeEndElement();
+
+                // Закрываем тэг Book
+                writer.writeEndElement();
+            }
+            // Закрываем корневой элемент
+            writer.writeEndElement();
+            // Закрываем XML-документ
+            writer.writeEndDocument();
+            writer.flush();
+        } catch (XMLStreamException | IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+//XPath
+class XPathExample {
+    public static void main(String[] args) {
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse("BookCatalogue.xml");
+
+            printCost(document);
+            printCost2(document);
+            printCost3(document);
+            printCost4(document);
+            printCost5(document);
+
+        } catch (XPathExpressionException | ParserConfigurationException | SAXException | IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    // Печать всех элементов Cost
+    private static void printCost(Document document) throws DOMException, XPathExpressionException {
+        System.out.println("Example 1 - Печать всех элементов Cost");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+
+        // Пример записи XPath
+        // Подный путь до элемента
+        //XPathExpression expr = xpath.compile("BookCatalogue/Book/Cost");
+        // Все элементы с таким именем
+        //XPathExpression expr = xpath.compile("//Cost");
+        // Элементы, вложенные в другой элемент
+        XPathExpression expr = xpath.compile("//Book/Cost");
+
+        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node n = nodes.item(i);
+            System.out.println("Value:" + n.getTextContent());
+        }
+        System.out.println();
+    }
+
+    // Печать элемента Cost у которого атрибут currency='USD'
+    private static void printCost2(Document document) throws DOMException, XPathExpressionException {
+        System.out.println("Example 2 - Печать элемента Cost у которого атрибут currency='USD'");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+        XPathExpression expr = xpath.compile("BookCatalogue/Book/Cost[@currency='USD']");
+        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node n = nodes.item(i);
+            System.out.println("Value:" + n.getTextContent());
+        }
+        System.out.println();
+    }
+
+    // Печать элементов Book у которых значение Cost > 4
+    private static void printCost3(Document document) throws DOMException, XPathExpressionException {
+        System.out.println("Example 3 - Печать элементов Book у которых значение Cost > 4");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+        XPathExpression expr = xpath.compile("BookCatalogue/Book[Cost>4]");
+        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node n = nodes.item(i);
+            System.out.println("Value:" + n.getTextContent());
+        }
+        System.out.println();
+    }
+
+    // Печать первого элемента Book
+    private static void printCost4(Document document) throws DOMException, XPathExpressionException {
+        System.out.println("Example 4 - Печать первого элемента Book");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+        XPathExpression expr = xpath.compile("BookCatalogue/Book[2]");
+        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node n = nodes.item(i);
+            System.out.println("Value:" + n.getTextContent());
+        }
+        System.out.println();
+    }
+
+    // Печать цены книги у которой Title начинается с Yogasana
+    // Варианты доступа к относительным узлам:
+    // ancestor , ancestor-or-self, descendant, descendant-or-self
+    // following, following-sibling, namespace, preceding, preceding-sibling
+    private static void printCost5(Document document) throws DOMException, XPathExpressionException {
+        System.out.println("Example 5 - Печать цены книги у которой Title начинается с 'Yogasana'");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+        XPathExpression expr = xpath.compile("BookCatalogue/Book/Cost"
+                + "[starts-with(preceding-sibling::Title, 'Yogasana')"
+                + " or "
+                + "starts-with(following-sibling::Title, 'Yogasana')]");
+        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node n = nodes.item(i);
+            System.out.println("Value:" + n.getTextContent());
+        }
+        System.out.println();
+    }
+
+}
+
+//XSD
+class XsdValidator {
+    public static void main(String[] args) {
+        boolean answer = validateXMLSchema("Message.xsd", "Message.xml");
+        System.out.println("Result:" + answer);
+    }
+
+    public static boolean validateXMLSchema(String xsdPath, String xmlPath) {
+        try {
+            // Получить фабрику для схемы
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            // Загрузить схему из XSD
+            Schema schema = factory.newSchema(new File(xsdPath));
+            // Создать валидатор (проверялбщик)
+            Validator validator = schema.newValidator();
+            // Запусить проверку - если будет исключение, значит есть ошибки.
+            // Если нет - все заполнено правильно
+            validator.validate(new StreamSource(new File(xmlPath)));
+        } catch (IOException | SAXException e) {
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
+
+//XSLT
 class XslConverter {
     public static void main(String[] arg) throws IOException {
         XslConverter c = new XslConverter();
