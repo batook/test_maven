e80fa23c41fc7fd48999f7735ac9b2c436ce254b
batook <batook@mail.ru>
2018-01-30 16:40:28 +0300

    work

--- src/main/java/CollectionsTest.java
+++ src/main/java/CollectionsTest.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
 public class CollectionsTest {
     public static void main(String[] args) {
 
     }
 }
+
+enum Color {RED, GREEN, BLUE, YELLOW, WHITE, BLACK}
 
 class TestArryList {
 
@@ -24,6 +30,57 @@ class TestHashMap {
 
 }
 
-class TestSortAndSearch {
+class TestLinkedHashMap {
 
+}
+
+class TestTreeMap {
+
+}
+
+// Ctrl+Q Quick Doc view
+// Ctrl+Shift+i Quick Defenition View
+class TestSortAndSearch {
+    static class SortArrays {
+        public static void main(String[] args) {
+            int[] ia = {5, 4, 3, 2, 1};
+            Arrays.sort(ia);
+            for (int i : ia) {
+                System.out.println(i);
+            }
+            String[] sa = "c b a".split(" ");
+            Arrays.sort(sa);
+            System.out.println(Arrays.asList(sa));
+
+            String[] sa2 = Pattern.compile("").split("abcdef");
+            Arrays.sort(sa2, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o2.compareTo(o1);
+                }
+            });
+            System.out.println(Arrays.asList(sa2));
+        }
+    }
+
+}
+
+class MyObject implements Comparable<MyObject> {
+    double d;
+    int i;
+    String s;
+    char c;
+
+    @Override
+    public int compareTo(MyObject o) {
+        int result;
+        result = Double.compare(d, o.d);
+        if (result != 0) return result;
+        result = Integer.compare(i, o.i);
+        if (result != 0) return result;
+        result = s.compareTo(o.s);
+        if (result != 0) return result;
+        result = Character.compare(c, o.c);
+        return result;
+    }
 }
