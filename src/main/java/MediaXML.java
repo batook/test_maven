4e9c04381acf887ae3e42941076f990e553821c0
batook <batook@mail.ru>
2018-02-03 01:51:21 +0300

    home

--- src/main/java/MediaXML.java
+++ src/main/java/MediaXML.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Objects;
 
 public class MediaXML {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class MediaXML {
 }
 
 class Item {
+    private String id;
     private List<Barcode> barcodes;
     private Disk disk;
     private String title;
@@ -16,7 +18,29 @@ class Item {
     private String description;
     private String type;
     private String genre;
-    private Boolean isHit;
+    private boolean isHit;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item that = (Item) o;
+        return Objects.equals(this.id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Item=" + id;
+    }
 
     public List<Barcode> getBarcodes() {
         return barcodes;
@@ -94,6 +118,28 @@ class Item {
 class Barcode {
     private String barcode;
 
+    public Barcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Barcode that = (Barcode) o;
+        return Objects.equals(this.barcode, that.barcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return barcode.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Barcode=" + barcode;
+    }
+
     public String getBarcode() {
         return barcode;
     }
@@ -106,6 +152,29 @@ class Barcode {
 class Disk {
     private List<Track> tracks;
     private int number;
+
+    public Disk(int number, List<Track> tracks) {
+        this.tracks = tracks;
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Disk that = (Disk) o;
+        return this.number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return number * 7;
+    }
+
+    @Override
+    public String toString() {
+        return "Disk=" + number;
+    }
 
     public List<Track> getTracks() {
         return tracks;
@@ -128,6 +197,31 @@ class Track {
     private int number;
     private String name;
     private String path;
+
+    public Track(int number, String name, String path) {
+        this.number = number;
+        this.name = name;
+        this.path = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Track that = (Track) o;
+        return this.number == that.number &&
+                Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return number * 7 ^ name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Track=" + number + "_" + name;
+    }
 
     public int getNumber() {
         return number;
