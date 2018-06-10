fd342e53d3dedf5f03ac7db94ab0562d5f43605b
batook <batook@mail.ru>
2018-06-10 10:43:09 +0300

    home

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,11 +1,39 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class TestTest {
 
+    @BeforeClass
+    public static void bc() {
+        System.out.println("0");
+    }
+
+    @Before
+    public void init() {
+        System.out.println("1");
+    }
+
+    @After
+    public void close() {
+        System.out.println("2");
+    }
+
     @Test
     public void go() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void go2() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void go3() {
         assertTrue(true);
     }
 }
