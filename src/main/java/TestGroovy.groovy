7ea08450c7b1632400cfc5c6b9b1c10f0913b2db
batook <batook@mail.ru>
2019-01-18 23:15:49 +0300

    home

---
+++ src/main/java/TestGroovy.groovy
@@ -0,0 +1,8 @@
+package com.batook;
+
+class Test {
+    static void main(String[] args) {
+        def t = 'shit'
+        println t
+    }
+}
--- src/test/java/com/batook/test/ArrayReversalDemoTest.java
+++ src/test/java/com/batook/test/ArrayReversalDemoTest.java
@@ -5,28 +5,28 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class ArrayReversalDemoTest {
-    @org.junit.Test
+    @Test
     public void testReverseWithEvenLengthOfArray() {
         int[] numbers = {1, 2, 3, 4, 5, 6};
         ArrayReversalDemo.reverse(numbers);
         assertArrayEquals(new int[]{6, 5, 4, 3, 2, 1}, numbers);
     }
 
-    @org.junit.Test
+    @Test
     public void testReverseWithOddLengthOfArray() {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         ArrayReversalDemo.reverse(numbers);
         assertArrayEquals(new int[]{7, 6, 5, 4, 3, 2, 1}, numbers);
     }
 
-    @org.junit.Test
+    @Test
     public void testReverseWithEmptyArray() {
         int[] numbers = {};
         ArrayReversalDemo.reverse(numbers);
         assertArrayEquals(new int[]{}, numbers);
     }
 
-    @org.junit.Test
+    @Test
     public void testReverseWithNullArray() {
         int[] numbers = null;
         ArrayReversalDemo.reverse(numbers);
